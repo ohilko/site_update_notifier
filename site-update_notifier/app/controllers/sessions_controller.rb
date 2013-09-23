27a7db942fc956b272c16a2255bc33d858252d55
Olga Hilko <ohilko@thumbtack.net>
2013-09-23 14:18:16 +0700

    Edit session

--- site-update_notifier/app/controllers/sessions_controller.rb
+++ site-update_notifier/app/controllers/sessions_controller.rb
@@ -4,17 +4,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.from_omniauth(env["omniauth.auth"])
-    
-    if user 
-      # Sign the user in and redirect to the user's show page.
-      session[:user_id] = user.id
-      redirect_to root_url, notice: "Signed in!"
-    else
-      flash.now[:error] = 'Invalid email/password combination'
-      render 'new'
-    end    
+    session[:user_id] = user.id
+    redirect_to root_url, notice: "Signed in!"
   end
-  
+
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "Signed out!"
