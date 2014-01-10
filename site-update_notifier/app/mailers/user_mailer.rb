d1725ffdf9e55155b445045b6ff6c3bc5e6e41d9
Olga Hilko <ohilko@thumbtack.net>
2014-01-10 18:11:18 +0700

    Work clockwork adn error when send email

--- site-update_notifier/app/mailers/user_mailer.rb
+++ site-update_notifier/app/mailers/user_mailer.rb
@@ -1,3 +1,5 @@
+# require File.expand_path('../../app/views/user_mailer/notifier_info_empty.html.erb', __FILE__)
+
 class UserMailer < ActionMailer::Base
 
   default :from => "from@example.com"
@@ -7,11 +9,19 @@ class UserMailer < ActionMailer::Base
   	mail to: user.email, subject: "Test"
   end
 
-  def notifier_info(user, hash_with_resources)
+  def notifier_info(user)
   	@user = user
   	# @resources = self.write_resources_in_table(user)
-  	@resources = hash_with_resources
+  	# @resources = hash_with_resources
     mail to: user.email, subject: "notifier"
+  end
+
+  def notifier_info_empty(user)
+    @user = user
+
+    mail(to: "to@example.com", subject: "notifier") do |format|
+      format.html { render user_mailer: 'notifier_info_empty.html.erb'}
+    end
   end
 
   def my_print
--- site-update_notifier/lib/notifier.rb
+++ site-update_notifier/lib/notifier.rb
@@ -1,82 +1,43 @@
 require 'rubygems'
 require 'bundler'
 Bundler.setup(:default, :ci)
-require 'active_record'
 
-require File.expand_path('../../app/models/resource', __FILE__)
+require 'active_record'
+require 'action_mailer'
 require 'clockwork'
-require 'rails/all'
 
 include Clockwork
 
-ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
-ActiveRecord::Schema.define do
-  self.verbose = false
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database  => "db/development.sqlite3"
+)
 
-  create_table :resources do |t|
-    t.string :url
-    t.string :name
-    t.integer :user_id
-    t.integer :timeout
-    t.boolean :change
-
-    t.timestamps
-  end
-end
+require File.expand_path('../../app/models/resource', __FILE__)
+require File.expand_path('../../app/models/user', __FILE__)
+require File.expand_path('../../app/mailers/user_mailer', __FILE__)
 
 module NotifierHelper
   # @@hash_with_resources = Hash.new 
 
-  # # send email all users about changer resources 
-  # def send_email(user)
-  # 	UserMailer.notifier_info(user, @@hash_with_resources).deliver
-  # end
+  # send email all users about changer resources 
+  def self.send_email(user)
+  	UserMailer.notifier_info_empty(user).deliver
+  end
 
-  # # list of resources which change
-  # def resources_of_change(user)
-  # 	user.resources
-  # end
+  def self.check_update(time)
+    flag = false
+    if time == 2
+      flag = true
+    end 
 
-  # # add new resource in table, which must to have information about all resources
-  # def add_resource_in_table(resource)
-  # 	@@hash_with_resources[:name].push(resource.name)
-  #   @@hash_with_resources[:url].push(resource.url)
-  #   @@hash_with_resources[:timeout].push(resource.timeout)
-  #   @@hash_with_resources[:change].push(resource.change)
-  #   @@hash_with_resources[:date_next_check].push(Time.now + resource.timeout * 60)
-  # end
-
-  # # write resources in table, it is very easy for use
-  # def write_resources_in_table(user)
-  # 	resources = Resource.all
-
-
-  # 	list_with_name = Array.new
-  # 	list_with_url = Array.new
-  # 	list_with_timeout = Array.new
-  # 	list_with_change = Array.new
-  # 	list_with_data_next_check = Array.new
-
-  # 	resources.each do |resource|
-  # 		list_with_name.push(resource.name)
-  # 		list_with_url.push(resource.url)
-  # 		list_with_timeout.push(resource.timeout)
-  # 		list_with_change.push(resource.change)
-  # 		list_with_data_next_check.push(Time.now + resource.timeout * 60)
-  # 	end
-
-  # 	@@hash_with_resources[:name] = list_with_name
-  #   @@hash_with_resources[:url] = list_with_url
-  # 	@@hash_with_resources[:timeout] = list_with_timeout
-  # 	@@hash_with_resources[:change] = list_with_change
-  # 	@@hash_with_resources[:date_next_check] = list_with_data_next_check
-
-  #   @@hash_with_resources
-  # end
+    flag
+  end
 
   def self.set_information_about_resources
     hash_with_resources = Hash.new
-    resources = Resource.where(name => 'google')
+    resources = Resource.all
+    # Resource.where(:name => 'google')
     # puts resources
 
     list_with_name = Array.new
@@ -106,7 +67,7 @@ module NotifierHelper
   end
 end
 
-hash_with_resources = Resource.where(name => 'google')
+hash_with_resources = NotifierHelper.set_information_about_resources
 
 puts hash_with_resources
 
@@ -114,7 +75,14 @@ list = hash_with_resources[:timeout]
 
 # list = [15, 20, 10]
 list.each do |element|
-  every(10.seconds, 'notifier.run') {
-    puts 'Good - ', element
+  every(element.seconds, 'notifier.run') {
+    flag = NotifierHelper.check_update(element)
+    if flag
+      puts '!!!!'
+    else
+      puts '????'
+    end
+    # puts 'Good - ', element
+    # NotifierHelper.send_email(User.where(:id => 2))
   }
 end
